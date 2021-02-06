a5dec4098331699de30e719164fd465ae8346cbd
SantiagoGuarneros <a17021089@iteshu.edu.mx>
2021-02-05 21:22:28 -0600

    Introduccion al uso de las apis peticion get

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import{RouterModule}from '@angular/router'
+import{RouterModule}from '@angular/router';
 
+//libreria para el uso de las apis configuracion en la linea 6 
+import{HttpClientModule}from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -23,6 +25,7 @@ import { ROUTES } from './app.routes';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot(ROUTES,{useHash:true})
   ],
   providers: [],
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  //constructor llamando  a las peticiones con las apis  http 
+
+  paises: any []=[];
+
+  constructor(private http: HttpClient) { 
+
+    this.http.get('https://api.covid19api.com/country/Argentina/status/confirmed?from=2020-12-01T00:00:00Z&to=2021-02-01T00:00:00Z')
+    .subscribe((res:any) =>{
+        this.paises=res;
+       console.log(res);
+    })
+    
+  }
 
   ngOnInit(): void {
   }
